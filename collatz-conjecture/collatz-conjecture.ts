17a19d4f6731c550ac6fc6b64bc62b9bf273e593
Invigo <invigo.devel@gmail.com>
2019-05-01 17:08:40 +0300

    collatz-conjecture solution

--- collatz-conjecture/collatz-conjecture.ts
+++ collatz-conjecture/collatz-conjecture.ts
@@ -1,12 +1,16 @@
-class CollatzConjecture {
+export default class CollatzConjecture {
+
     static steps(num: number) {
+        
         if (num <= 0) {
             throw new Error('Only positive numbers are allowed')
         }
-        const iter: number = (n: number, s: number) => (n === 1) ? s :
-                             (n % 2 === 0) ? iter(n / 2, s + 1) : iter((3 * n) + 1, s + 1)
+
+        function iter(n: number, s: number): number {
+            return (n === 1) ? s : (n % 2 === 0) ? iter(n / 2, s + 1)
+                                                 : iter((3 * n) + 1, s + 1)
+        }
 
         return iter(num, 0)
+    }
 }
-
-export default CollatzConjecture
