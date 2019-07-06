901d68f44f8698e7894b8b5ad2b6a17101ec2b62
Invigo <invigo.devel@gmail.com>
2019-07-06 17:07:09 +0300

    matching-brackets solution

---
+++ matching-brackets/matching-brackets.ts
@@ -0,0 +1,19 @@
+export default class MatchingBrackets {
+
+  bracketsOnly: string
+
+  constructor(input: string) {
+    this.bracketsOnly = input.replace(/[^\[\]|\(\)|\{\}]/g, '')
+  }
+
+  isPaired(): Boolean {
+    let allBrackets = this.bracketsOnly
+    let matchAndStrip
+    do {
+      matchAndStrip = allBrackets
+      allBrackets = matchAndStrip.replace(/\[\]|\(\)|\{\}/g, '')
+    } while (allBrackets !== matchAndStrip)
+    return allBrackets === ''
+  }
+
+}
