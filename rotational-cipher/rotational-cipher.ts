0b259b67d2bf5dd21ab65785ee96032f59de40a5
Invigo <invigo.devel@gmail.com>
2020-04-09 23:33:33 +0300

    Test updated rotational-cipher

--- rotational-cipher/rotational-cipher.ts
+++ rotational-cipher/rotational-cipher.ts
@@ -1,11 +1,16 @@
 const shift = (char: string, base: number , key: number) =>
-    String.fromCharCode((char.charCodeAt(0) - base + key) % 26 + base)
+    String
+    .fromCharCode((char.charCodeAt(0) - base + key) % 26 + base)
 
 export default class RotationalCipher {
 
     static rotate  (text: string, key: number) {
         return text
-               .replace (/[a-z]/g, (char) => shift (char, 'a'.charCodeAt(0), key))
-               .replace (/[A-Z]/g, (char) => shift (char, 'A'.charCodeAt(0), key))
-    }    
+               .replace(/[a-z]/g,
+                        (char) =>
+                            shift (char, 'a'.charCodeAt(0), key))
+               .replace(/[A-Z]/g,
+                        (char) =>
+                            shift (char, 'A'.charCodeAt(0), key))
+    }
 }
