739d6f76ee6779f12a3635620f4ae827ff8c479d
Invigo <invigo.devel@gmail.com>
2020-11-30 14:04:10 +0200

    Update difference-of-squares.ts

--- difference-of-squares/difference-of-squares.ts
+++ difference-of-squares/difference-of-squares.ts
@@ -1,12 +1,15 @@
 export default class Squares {
 
-  squareOfSums: number
+  squareOfSum: number
   sumOfSquares: number
   difference: number
 
   constructor (num: number) {
-      this.squareOfSums = (num * (num + 1) / 2) ** 2
-      this.sumOfSquares = (num ** 3) / 3 + (num ** 2) / 2 + num / 6 
-      this.difference   = this.squareOfSums - this.sumOfSquares
+      this.squareOfSum =
+          (num * (num + 1) / 2) ** 2
+      this.sumOfSquares =
+          (num ** 3) / 3 + (num ** 2) / 2 + num / 6
+      this.difference   =
+          this.squareOfSum - this.sumOfSquares
   }
 }
