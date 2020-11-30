f205326515b289d4d07d0b41a84958f74ee46ca6
Invigo <invigo.devel@gmail.com>
2020-11-30 14:04:58 +0200

    Update word-count.ts

--- word-count/word-count.ts
+++ word-count/word-count.ts
@@ -3,11 +3,13 @@ export default class Words {
   constructor () {}
 
   count (sentence: String): Map<string, number> {
-      return sentence.trim()
-                     .toLowerCase()
-                     .split(/\s+/)
-                     .reduce((nums, word) =>
-                        nums.set(word, (nums.get(word) || 0) + 1), new Map()
-                      )
+      return sentence
+             .trim()
+             .toLowerCase()
+             .split(/\s+/)
+             .reduce((nums, word) =>
+                      nums.set(word,
+                               (nums.get(word) || 0) + 1),
+                               new Map())
   }
 }
