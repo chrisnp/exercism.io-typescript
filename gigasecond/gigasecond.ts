8a2b6918f543ed3a315a01a53ec9b9fe80aaa4dc
Invigo <invigo.devel@gmail.com>
2020-11-30 14:06:09 +0200

    Refactor gigasecond

--- gigasecond/gigasecond.ts
+++ gigasecond/gigasecond.ts
@@ -1,11 +1,15 @@
-const GIGASECOND = 1e09 
+const GIGASECOND = 1e09
 const MILS = 1e03
 
 export default class Gigasecond {
 
-    constructor (private readonly dob: Date) {
+    private readonly dob: Date
+
+    constructor (dob: Date) {
         this.dob = dob
     }
 
-    date = (): Date => new Date(this.dob.getTime() + MILS * GIGASECOND)
+    date = (): Date =>
+                    new Date(this.dob.getTime() +
+                        MILS * GIGASECOND)
 }
