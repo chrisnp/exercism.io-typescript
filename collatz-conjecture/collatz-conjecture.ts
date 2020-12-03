630b569c49174623e4f9679e07706cc6944b4506
Invigo <invigo.devel@gmail.com>
2020-12-03 14:41:32 +0200

    Update collatz-conjecture.ts

--- collatz-conjecture/collatz-conjecture.ts
+++ collatz-conjecture/collatz-conjecture.ts
@@ -3,7 +3,8 @@ export default class CollatzConjecture {
     static steps(num: number) {
 
         if (num <= 0) {
-            throw new Error('Only positive numbers are allowed')
+            throw new 
+                Error('Only positive numbers are allowed')
         }
 
         function iter(n: number, s: number): number {
