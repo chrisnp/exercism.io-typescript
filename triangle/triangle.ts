10041d71dcfc63be99315101da0e4a833868799f
Invigo <invigo.devel@gmail.com>
2020-12-04 17:16:42 +0200

    Update triangle.ts

--- triangle/triangle.ts
+++ triangle/triangle.ts
@@ -3,13 +3,15 @@ export default class Triangle {
     sides: number[]
 
     constructor(...sides: number[]) {
-        const order = (x: number, y: number) => x - y
+        const order = (x: number, y: number) =>
+                                        x - y
         this.sides = sides.sort(order)
     }
 
     kind() {
         this.throwWhatever()
-        const distinctSides = (new Set(this.sides)).size
+        const distinctSides = 
+            (new Set(this.sides)).size
         switch (distinctSides) {
             case 1: return 'equilateral'
             case 2: return 'isosceles'
@@ -18,7 +20,11 @@ export default class Triangle {
     }
 
     throwWhatever() {
-        if (this.sides.some((side) => side <= 0) || this.sides[2] > (this.sides[0] + this.sides[1])) {
+        if (this.sides.some((side) =>
+                                side <= 0)
+            ||
+            this.sides[2] >
+            (this.sides[0] + this.sides[1])) {
             throw new Error('Illegal')
         }
     }
