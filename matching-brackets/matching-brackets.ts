3f79cf62eca4b3c4ed62470880aa3efbcaf48a13
Invigo <invigo.devel@gmail.com>
2020-12-10 21:15:46 +0200

    Update matching-brackets.ts

--- matching-brackets/matching-brackets.ts
+++ matching-brackets/matching-brackets.ts
@@ -3,15 +3,19 @@ export default class MatchingBrackets {
   justTheBrackets: string
 
   constructor(input: string) {
-    this.justTheBrackets = input.replace(/[^\[\]|\(\)|\{\}]/g, '')
+    this.justTheBrackets = 
+        input.replace(/[^\[\]|\(\)|\{\}]/g, '')
   }
 
   isPaired(): Boolean {
-    let allBrackets = this.justTheBrackets
+    let allBrackets = 
+        this.justTheBrackets
     let matchAndStrip
     do {
-      matchAndStrip = allBrackets
-      allBrackets = matchAndStrip.replace(/\[\]|\(\)|\{\}/g, '')
+      matchAndStrip = 
+            allBrackets
+      allBrackets = 
+          matchAndStrip.replace(/\[\]|\(\)|\{\}/g, '')
     } while (allBrackets !== matchAndStrip)
     return allBrackets === ''
   }
