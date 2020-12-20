67968688512643bfaa69722fddc13a4ffadd9982
Invigo <invigo.devel@gmail.com>
2020-12-20 23:23:57 +0200

    Create rna-transcription.ts

---
+++ rna-transcription/rna-transcription.ts
@@ -0,0 +1,23 @@
+const TRANSL: { [key: string]: string } = {
+    "G": "C",
+    "C": "G",
+    "T": "A",
+    "A": "U"
+};
+
+class Transcriptor {
+
+    toRna(dna: string): string {
+        if (/[^ACGT]/.test(dna)) {
+            throw new
+                Error("Invalid input DNA.");
+        } else {
+            return dna
+                   .split("")
+                   .map((n) => TRANSL[n])
+                   .join("");
+        }
+    }
+}
+
+export default Transcriptor;
