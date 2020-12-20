4002bfc866f5fbe7f9594da2a20a52af15d61a5f
Invigo <invigo.devel@gmail.com>
2020-12-20 23:41:33 +0200

    Refactor leap.ts

--- leap/leap.ts
+++ leap/leap.ts
@@ -1,7 +1,6 @@
-function isLeapYear( year: number ): boolean {
-    return year % 100 === 0 ?
-           year % 400 === 0 :
-           year % 4   === 0
-}
+const isLeapYear = (year: number): boolean =>
+    year % 100 === 0 ?
+    year % 400 === 0 :
+    year % 4   === 0
 
 export default isLeapYear
