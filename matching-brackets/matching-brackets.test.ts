c15b31f0e6c0b500439c086455a2e83f2f56cb60
Invigo <invigo.devel@gmail.com>
2021-04-09 23:22:49 +0300

    Update matching-brackets.test.ts

--- matching-brackets/matching-brackets.test.ts
+++ matching-brackets/matching-brackets.test.ts
@@ -1,76 +1,76 @@
-import MatchingBrackets from "./matching-brackets"
+import MatchingBrackets from './matching-brackets'
 
-describe("Matching Brackets", () => {
-  it("paired square brackets", () => {
-    const matchingBrackets = new MatchingBrackets("[]")
+describe('Matching Brackets', () => {
+  it('paired square brackets', () => {
+    const matchingBrackets = new MatchingBrackets('[]')
     expect(matchingBrackets.isPaired()).toBeTruthy()
   })
 
-  it("empty string", () => {
-    const matchingBrackets = new MatchingBrackets("")
+  xit('empty string', () => {
+    const matchingBrackets = new MatchingBrackets('')
     expect(matchingBrackets.isPaired()).toBeTruthy()
   })
 
-  it("unpaired brackets", () => {
-    const matchingBrackets = new MatchingBrackets("[[")
+  xit('unpaired brackets', () => {
+    const matchingBrackets = new MatchingBrackets('[[')
     expect(matchingBrackets.isPaired()).toBeFalsy()
   })
 
-  it("wrong ordered brackets", () => {
-    const matchingBrackets = new MatchingBrackets("}{")
+  xit('wrong ordered brackets', () => {
+    const matchingBrackets = new MatchingBrackets('}{')
     expect(matchingBrackets.isPaired()).toBeFalsy()
   })
 
-  it("wrong closing bracket", () => {
-    const matchingBrackets = new MatchingBrackets("{]")
+  xit('wrong closing bracket', () => {
+    const matchingBrackets = new MatchingBrackets('{]')
     expect(matchingBrackets.isPaired()).toBeFalsy()
   })
 
-  it("paired with whitespace", () => {
-    const matchingBrackets = new MatchingBrackets("{ }")
+  xit('paired with whitespace', () => {
+    const matchingBrackets = new MatchingBrackets('{ }')
     expect(matchingBrackets.isPaired()).toBeTruthy()
   })
 
-  it("simple nested brackets", () => {
-    const matchingBrackets = new MatchingBrackets("{[]}")
+  xit('simple nested brackets', () => {
+    const matchingBrackets = new MatchingBrackets('{[]}')
     expect(matchingBrackets.isPaired()).toBeTruthy()
   })
 
-  it("several paired brackets", () => {
-    const matchingBrackets = new MatchingBrackets("{}[]")
+  xit('several paired brackets', () => {
+    const matchingBrackets = new MatchingBrackets('{}[]')
     expect(matchingBrackets.isPaired()).toBeTruthy()
   })
 
-  it("paired and nested brackets", () => {
-    const matchingBrackets = new MatchingBrackets("([{}({}[])])")
+  xit('paired and nested brackets', () => {
+    const matchingBrackets = new MatchingBrackets('([{}({}[])])')
     expect(matchingBrackets.isPaired()).toBeTruthy()
   })
 
-  it("unopened closing brackets", () => {
-    const matchingBrackets = new MatchingBrackets("{[)][]}")
+  xit('unopened closing brackets', () => {
+    const matchingBrackets = new MatchingBrackets('{[)][]}')
     expect(matchingBrackets.isPaired()).toBeFalsy()
   })
 
-  it("unpaired and nested brackets", () => {
-    const matchingBrackets = new MatchingBrackets("([{])")
+  xit('unpaired and nested brackets', () => {
+    const matchingBrackets = new MatchingBrackets('([{])')
     expect(matchingBrackets.isPaired()).toBeFalsy()
   })
 
-  it("paired and wrong nested brackets", () => {
-    const matchingBrackets = new MatchingBrackets("[({]})")
+  xit('paired and wrong nested brackets', () => {
+    const matchingBrackets = new MatchingBrackets('[({]})')
     expect(matchingBrackets.isPaired()).toBeFalsy()
   })
 
-  it("math expression", () => {
+  xit('math expression', () => {
     const matchingBrackets = new MatchingBrackets(
-      "(((185 + 223.85) * 15) - 543)/2"
+      '(((185 + 223.85) * 15) - 543)/2'
     )
     expect(matchingBrackets.isPaired()).toBeTruthy()
   })
 
-  it("complex latex expression", () => {
+  xit('complex latex expression', () => {
     const matchingBrackets = new MatchingBrackets(
-      "\\left(\\begin{array}{cc} \\frac{1}{3} & x\\\\ \\mathrm{e}^{x} &... x^2 \\end{array}\\right)"
+      '\\left(\\begin{array}{cc} \\frac{1}{3} & x\\\\ \\mathrm{e}^{x} &... x^2 \\end{array}\\right)'
     )
     expect(matchingBrackets.isPaired()).toBeTruthy()
   })
