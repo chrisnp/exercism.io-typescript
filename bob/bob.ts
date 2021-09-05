20155302f5f6b2bf1248b51f54cd040cd0f6ae93
Invigo <invigo.devel@gmail.com>
2021-09-06 01:50:10 +0300

    bob  - 1st passing iteration

--- bob/bob.ts
+++ bob/bob.ts
@@ -1,3 +1,19 @@
-export function hey(message: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
-}
+const shouting = (message: string):boolean =>
+            message === message.toUpperCase() && 
+            (/[A-Z]+/).test(message),
+      question = (message: string):boolean =>
+            message.trim().endsWith('?'),
+      silence  = (message: string):boolean =>
+            !message.trim(),
+      forceful = (message: string):boolean =>
+            shouting(message) && question(message)
+
+
+export const hey = (message: string): string => 
+            forceful(message) ? 
+                "Calm down, I know what I'm doing!"  :
+            shouting(message) ? "Whoa, chill out!"   :
+            question(message) ? "Sure."              :
+            silence (message) ? "Fine. Be that way!" :
+                                "Whatever."
+
