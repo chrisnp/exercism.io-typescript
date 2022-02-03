4cf78eeef8e3f6bd812afee7012f8a286fba99a0
Invigo <invigo.devel@gmail.com>
2022-02-03 14:41:33 +0200

    Create binary-search.ts

---
+++ binary-search/binary-search.ts
@@ -0,0 +1,12 @@
+export function find<T>(haystack: T[], needle: T): number | never {
+    let lo = 0,
+        hi = haystack.length - 1
+
+    while (lo <= hi) {
+        const mid = (hi + lo) >> 1
+        if (haystack[mid] === needle) return mid
+        haystack[mid] > needle ? hi = mid - 1 : lo = mid + 1
+    }
+
+    throw ('Value not in array')
+}
