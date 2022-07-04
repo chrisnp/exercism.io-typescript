7682fe64f7e37d9cc484ccfcaa0d4904d9d1df78
Invigo <invigo.devel@gmail.com>
2022-07-04 18:18:42 +0300

    Refactor queen-attack.ts

--- queen-attack/queen-attack.ts
+++ queen-attack/queen-attack.ts
@@ -1,48 +1,42 @@
-interface IPosition {
-  readonly black: number[]
-  readonly white: number[]
-}
+type Queen = readonly [number, number]
 
-export class QueenAttack implements IPosition{
+type Position = { white: Queen; black: Queen }
 
-  public readonly black: number[]
-  public readonly white: number[]
+export class QueenAttack {
+  readonly black: Queen
+  readonly white: Queen
 
-  constructor(position: IPosition = {black: [0, 0], white: [0, 0]}) {
-
-    const { black: [bfile, brank], white: [wfile, wrank] } = position
-
-    this.black = position.black
-    this.white = position.white
-
+  constructor({ black, white }: Partial<Position> = {}) 
+  {
+    this.black = black || [0, 3]
+    this.white = white || [7, 3]
+    const [bfile, brank] = this.black 
+    const [wfile, wrank] = this.white 
+    if ([bfile, brank, wfile, wrank].some(p => p < 0 || p > 7)) {
+        throw new Error('Queen must be placed on the board')
+    }
     if (bfile === wfile && brank === wrank) {
       throw new Error("Queens cannot share the same space")
     }
-  }
-
-  get position(): IPosition {
-    return [this., this.col]
   }
 
   toString(): string {
     const [wfile, wrank] = this.white
     const [bfile, brank] = this.black
     const board = Array.from(Array(8), () => Array(8).fill('_'))
-
     board[bfile][brank] = 'B'
     board[wfile][wrank] = 'W'
-
-    return board.map(row => row.join(' '))
-                .join('\n') + "\n"
+    return board.map(row => row.join(' ')).join('\n')
   }
 
-  canAttack(): boolean {
+  get canAttack(): boolean {
     const [wfile, wrank] = this.white
     const [bfile, brank] = this.black
     const dfile = Math.abs(wfile - bfile)
     const drank = Math.abs(wrank - brank)
-
     return dfile * drank === 0 || 
            dfile / drank === 1
   }
 }
+
+
