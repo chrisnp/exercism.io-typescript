028bb3518db72e8cddddcb081e0f21806f30f2fc
Invigo <invigo.devel@gmail.com>
2022-07-05 23:33:53 +0300

    Reformat simple-cipher.ts

--- simple-cipher/simple-cipher.ts
+++ simple-cipher/simple-cipher.ts
@@ -3,21 +3,28 @@ const ALPHA = 'abcdefghijklmnopqrstuvwxyz'
 const mod = (x: number, y: number): number => (x % y + y) % y
 
 export class SimpleCipher {
-
     public readonly key: string
 
     constructor(key?: string) {
-
-        if (key === undefined) {
-            key = this.generateKey()
+        if (key === undefined) { 
+            key = this.generateKey() 
         }
-        else if (key.length === 0 || key.match(/[^a-z]/)) {
-            throw new Error('Bad key')
+        else 
+        if (key.length === 0 || key.match(/[^a-z]/)) { 
+            throw new Error('Bad key') 
         }
         this.key = key
     }
 
-    private dShift(key: string, input: string, direction: number): string {
+    encode(plaintext: string ): string {
+        return this.shift(this.key, plaintext, 1)
+    }
+
+    decode(ciphertext: string): string {
+        return this.shift(this.key, ciphertext, -1)
+    }
+
+    private shift(key: string, input: string, direction: number): string {
         return [...input].reduce((output, ch, i) => {
                 const offset = direction * ALPHA.indexOf(key[mod(i, key.length)])
                 output += ALPHA[mod(ALPHA.indexOf(ch) + offset, ALPHA.length)]
@@ -26,16 +33,8 @@ export class SimpleCipher {
     }
 
     private generateKey(): string {
-        return Array(...Array(100)).map(() => 
-                                        ALPHA[~~(Math.random() * ALPHA.length)])
-                                   .join('')
-    }
-
-    encode(plaintext: string ): string {
-        return this.dShift(this.key, plaintext, 1)
-    }
-
-    decode(ciphertext: string): string {
-        return this.dShift(this.key, ciphertext, -1)
+        return Array(...Array(100))
+               .map(() => ALPHA[~~(Math.random() * ALPHA.length)])
+               .join('')
     }
 }
