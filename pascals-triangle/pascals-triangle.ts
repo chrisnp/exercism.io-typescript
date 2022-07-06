58e0163ca0a0f0f741c55a2ef63d6508a2764b6b
Invigo <invigo.devel@gmail.com>
2022-07-06 19:58:07 +0300

    1st passing iteration - pascals-triangle.ts

--- pascals-triangle/pascals-triangle.ts
+++ pascals-triangle/pascals-triangle.ts
@@ -1,1 +1,21 @@
-export class Triangle {}
+const factorial = (n: number): number => 
+            n <= 1 
+            ? 1 
+            : n * factorial (n - 1), 
+      binomial = (n: number, k: number): number => 
+            factorial (n) / (factorial (k) * factorial (n - k))
+
+export class Triangle {
+    rows: number[][]
+    lastRow: number[]
+
+    constructor (n: number) {
+        this.rows = 
+            [...Array(n)].map((_, i) => 
+                [...Array(i + 1)].map((_, j) => 
+                    binomial (i, j)
+                )
+            )
+        this.lastRow = this.rows[this.rows.length - 1]
+    }
+}
