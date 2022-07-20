824736b43b081c2871095931e80a6b7d1f46c790
Invigo <invigo.devel@gmail.com>
2022-07-20 19:48:12 +0300

    prime-factors.ts - passing all tests

--- prime-factors/prime-factors.ts
+++ prime-factors/prime-factors.ts
@@ -1,3 +1,18 @@
-export function calculatePrimeFactors() {
-  throw new Error('Remove this statement and implement this function')
+export const calculatePrimeFactors = 
+    (number: number): number[] => {
+        const primeFactors = []
+        let candidate = 2
+        while ( number > 1 ) {
+            if ( number % candidate === 0 ) {
+                primeFactors.push (candidate)
+                number /= candidate
+            } 
+            else {
+                candidate++
+                if ( candidate % 2 === 0 ) {
+                    candidate++
+                }
+            }
+        }
+        return primeFactors
 }
