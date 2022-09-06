39c7edd96f6ca70a52acdbe06ab40cc46d5502fb
Invigo <invigo.devel@gmail.com>
2022-09-06 14:34:41 +0300

    Refactor triangle

--- triangle/triangle.ts
+++ triangle/triangle.ts
@@ -1,38 +1,34 @@
 export class Triangle {
-
     sides: number[]
-    
     constructor (...sides: number[]) {
         const order = (x: number, y: number) => x - y
         this.sides = sides.sort(order)
     }
-
-    kind = (): string => {
-        this.throwWhatever()
-        const distinctSides = 
-            (new Set(this.sides)).size
-        switch (distinctSides) {
-            case 1:  return 'equilateral'
-            case 2:  return 'isosceles'
-            case 3:  return 'scalene'
-            default: return 'degenerate'
-        }
-    }
-
-    get isEquilateral(): boolean { return this.kind() === 'equilateral' }
-    get isIsosceles(): boolean { return this.kind() === 'isosceles' }
-    get isScalene(): boolean { return this.kind() === 'scalene' }
-
     triangleInequality = (): boolean => 
         2 * Math.max(...this.sides) <
         this.sides.reduce((sum, s) => sum + s, 0); 
-
     anySideZero = (): boolean =>
         this.sides.some((s) => s <= 0); 
-
     throwWhatever = (): boolean => { 
         if ( !this.triangleInequality() || this.anySideZero() ) 
-               throw new Error('Illegal')
-        return true
+               return true
+        return false
+    }
+    distinct = (): number => {
+        const distinctSides = 
+            (new Set(this.sides)).size
+        return distinctSides
+    }
+    get isEquilateral (): boolean { 
+        return !this.throwWhatever() && 
+        this.distinct() === 1 
+    }
+    get isIsosceles (): boolean { 
+        return !this.throwWhatever() && 
+        this.distinct() <= 2 
+    }
+    get isScalene (): boolean { 
+        return !this.throwWhatever() && 
+        this.distinct() === 3 
     }
 }
