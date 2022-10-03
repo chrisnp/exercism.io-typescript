b001c42080e6781bf94c84ac78d83190f8410f6e
Invigo <invigo.devel@gmail.com>
2022-10-03 23:10:48 +0300

    resistor-color-trio.ts - 1st passing iteration

--- resistor-color-trio/resistor-color-trio.ts
+++ resistor-color-trio/resistor-color-trio.ts
@@ -1,3 +1,27 @@
-export function decodedResistorValue() {
-  throw new Error('Remove this statement and implement this function')
+const COLORS = [ 'black',
+                 'brown',
+                 'red',
+                 'orange',
+                 'yellow',
+                 'green',
+                 'blue',
+                 'violet',
+                 'grey',
+                 'white' ]
+
+export type Colour = typeof COLORS[number]
+
+export function decodedResistorValue([b1, b2, b3]: Colour[]) : string {
+  const colours = [b1, b2, b3]
+  if (colours === undefined || colours.length < 3)
+    throw new Error("there must be at least 3 colours")
+  const codes = colours.map(c => COLORS.indexOf(c))
+  if (codes.some(code => code < 0)) {
+    throw new Error("invalid colour")
+  }
+  const resistorValue = ((10 * codes[0]) + codes[1]) * (10 ** codes[2])
+  const {value, units} = resistorValue > 1000 ? 
+                         {value: resistorValue / 1000, units: `kiloohms`} : 
+                         {value: resistorValue, units: `ohms`}
+  return `${value} ${units}`
 }
