5191a7ab726dd6446b4b6df5e11c7ee8416ba8be
Invigo <invigo.devel@gmail.com>
2022-10-10 00:36:16 +0300

    Refactor luhn

--- luhn/luhn.ts
+++ luhn/luhn.ts
@@ -1,9 +1,11 @@
 export const valid = (digitString: string): boolean => {
     digitString = digitString.replace(/\s/g, '')
     const digits = [...digitString]
-    const luhn = digits.map(d => parseInt(d, 10))
-                       .map((d, i) => ((i % 2) !== 0) ? d * 2 : d)
-                       .map(d => (d > 9) ? d - 9 : d)
-                       .reduce((sum, d) => sum + d, 0)
+    const luhn = 
+        digits.map(d => parseInt(d, 10))
+              .map((d, i) => 
+                    (i % 2) !== 0 ? d << 1 : d)
+              .map(d => (d > 9) ? d - 9 : d)
+              .reduce((sum, d) => sum + d, 0)
     return luhn > 0 && luhn % 10 === 0
 }
