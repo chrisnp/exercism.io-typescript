b38431018fbcf166fafe979fec840e127f05de41
Invigo <invigo.devel@gmail.com>
2022-10-10 00:36:40 +0300

    Update resistor-color-duo.ts

--- resistor-color-duo/resistor-color-duo.ts
+++ resistor-color-duo/resistor-color-duo.ts
@@ -17,14 +17,12 @@ const LESS_THAN_TWO_COLORS = "At least two colors need to be present";
 
 class ResistorColor {
   private colors: Color[];
-
   constructor(colors: Color[]) {
     if (colors.length < 2) {
       throw new Error(LESS_THAN_TWO_COLORS);
     }
     this.colors = colors.slice(0, 2);
   }
-
   value = (): number => {
     const [c1, c2] = this.colors.map(c => Colors[c]);
     return 10 * c1 + c2;
