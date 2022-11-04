66866c87157352f9d1b2cb232fc6367a9b6e1983
Invigo <invigo.devel@gmail.com>
2022-11-04 17:23:10 +0200

    sublist.ts passing all tests

--- sublist/sublist.ts
+++ sublist/sublist.ts
@@ -1,9 +1,36 @@
 export class List {
-  constructor() {
-    throw new Error('Remove this statement and implement this function')
+  private list: any[]
+
+  constructor(...items: any[]) {
+    this.list = items
   }
 
-  compare() {
-    throw new Error('Remove this statement and implement this function')
+  compare(that: List): string {
+    const sameSize = this.list.length === that.list.length
+
+    if (sameSize && this.contains(this, that)) { 
+      return 'equal' 
+    } else if (this.contains(that, this)) {
+      return 'sublist'
+    } else if (this.contains(this, that)) {
+      return 'superlist'
+    } else {
+      return 'unequal'
+    }
+  }
+
+  contains(sup: List, sub: List): boolean {
+    return sub.list.length === 0 || 
+           sup.list.some((_, supIdx) => 
+            sub.list.every((subElem, subIdx) => 
+              subElem === sup.list[supIdx + subIdx]))
   }
 }
+
+// function contains<T> (sup: T[], sub: T[]): boolean {
+//   return sup === [] 
+//          ? false 
+//          : sub === [] 
+//          ? true 
+//          : sub.filter(x => !(x in sup)).length === 0
+// }
