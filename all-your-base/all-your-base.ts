0aed199f129fba73a95ce0563fa9235c20b277f0
Invigo <invigo.devel@gmail.com>
2023-07-13 21:31:12 +0300

    Refactor all-your-base

--- all-your-base/all-your-base.ts
+++ all-your-base/all-your-base.ts
@@ -1,41 +1,21 @@
-function validBase(base: number) {
-    return  typeof(base) === 'number' &&
-            ~~base === base && base > 1
-}
+const validBase = (base: number) =>
+    typeof(base) === 'number' && ~~base === base && base > 1
 
-function validInput(input: number[], fromBase: number) {
-    return input.length > 0 &&
-            !(input.length > 1 && input[0] === 0) &&
-            input.every((d) => 0 <= d &&
-                        d < fromBase)
-}
+const validInput = (input: number[], fromBase: number) =>
+    input.length > 0 && !(input.length > 1 && input[0] === 0) &&
+    input.every((d) => 0 <= d && d < fromBase)
 
-export default class Converter {
-
-    convert (input: number[], fromBase: number, toBase: number) {
-
-        if (!validBase(fromBase)) {
-            throw new Error("Wrong input base")
-        }
-
-        if (!validBase(toBase)) {
-            throw new Error("Wrong output base")
-        }
-
-        if (!validInput (input, fromBase)) {
-            throw new Error("Input has wrong format")
-        }
-
-        let num =
-            input.reduce((acc, x) => acc * fromBase + x, 0)
-
-        const digits = []
-
-        do {
-            digits.unshift(num % toBase)
-            num = ~~(num / toBase)
-        } while (num > 0)
-
-        return digits
-    }
+export const convert = (input: number[], fromBase: number, toBase: number) => {
+    if (!validBase(fromBase)) { throw new Error("Wrong input base") }
+    if (!validBase(toBase)) { throw new Error("Wrong output base") }
+    if (!validInput (input, fromBase)) { 
+        throw new Error("Input has wrong format") 
+    } 
+    let num = input.reduce((acc, x) => acc * fromBase + x, 0)
+    const digits = []
+    do {
+        digits.unshift(num % toBase)
+        num = ~~(num / toBase)
+    } while (num > 0)
+    return digits
 }
