d7491abfbd3217ddadfa9cff18f9bb8da4bee875
Invigo <invigo.devel@gmail.com>
2023-07-19 23:56:02 +0300

    Update collatz-conjecture.test.ts

--- collatz-conjecture/collatz-conjecture.test.ts
+++ collatz-conjecture/collatz-conjecture.test.ts
@@ -1,37 +1,44 @@
-import CollatzConjecture from './collatz-conjecture'
+import { steps } from './collatz-conjecture'
 
 describe('CollatzConjecture', () => {
   it('zero steps for one', () => {
     const expected = 0
-    expect(CollatzConjecture.steps(1)).toBe(expected)
+    expect(steps(1)).toBe(expected)
   })
 
-  it('divide if even', () => {
+  xit('divide if even', () => {
     const expected = 4
-    expect(CollatzConjecture.steps(16)).toBe(expected)
+    expect(steps(16)).toBe(expected)
   })
 
-  it('even and odd steps', () => {
+  xit('even and odd steps', () => {
     const expected = 9
-    expect(CollatzConjecture.steps(12)).toBe(expected)
+    expect(steps(12)).toBe(expected)
   })
 
-  it('Large number of even and odd steps', () => {
+  xit('Large number of even and odd steps', () => {
     const expected = 152
-    expect(CollatzConjecture.steps(1000000)).toBe(expected)
+    expect(steps(1000000)).toBe(expected)
   })
 
-  it('zero is an error', () => {
-    const expected = 'Only positive numbers are allowed'
+  xit('zero is an error', () => {
+    const expected = 'Only positive integers are allowed'
     expect(() => {
-      CollatzConjecture.steps(0)
+      steps(0)
     }).toThrowError(expected)
   })
 
-  it('negative value is an error', () => {
-    const expected = 'Only positive numbers are allowed'
+  xit('negative value is an error', () => {
+    const expected = 'Only positive integers are allowed'
     expect(() => {
-      CollatzConjecture.steps(-15)
+      steps(-15)
+    }).toThrowError(expected)
+  })
+
+  xit('non-integer value is an error', () => {
+    const expected = 'Only positive integers are allowed'
+    expect(() => {
+      steps(3.1415)
     }).toThrowError(expected)
   })
 })
