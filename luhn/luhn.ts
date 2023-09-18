f60dd1f2fe7aa1a02eb1453c6389fe5a39d28789
Invigo <invigo.devel@gmail.com>
2023-09-18 23:14:27 +0300

    Update luhn.ts

--- luhn/luhn.ts
+++ luhn/luhn.ts
@@ -1,11 +1,12 @@
 export const valid = (digitString: string): boolean => {
     digitString = digitString.replace(/\s/g, '')
-    const digits = [...digitString]
-    const luhn = 
-        digits.map(d => parseInt(d, 10))
-              .map((d, i) => 
-                    (i % 2) !== 0 ? d << 1 : d)
-              .map(d => (d > 9) ? d - 9 : d)
-              .reduce((sum, d) => sum + d, 0)
+    if (digitString.length <= 1 || /\D/.test(digitString))
+        return false
+    const digits = [...digitString].reverse()
+    const luhn = digits.map(d => parseInt(d, 10))
+                       .map((d, i) => 
+                            !(i & 2) ? (d << 1) - (d > 4 ? 9 : 0) : d)
+                    //    .map(d => (d > 9) ? d - 9 : d)
+                       .reduce((sum, d) => sum + d, 0)
     return luhn > 0 && luhn % 10 === 0
 }
