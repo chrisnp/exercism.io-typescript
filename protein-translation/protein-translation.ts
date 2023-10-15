b323807d348699a51275f3c89bb73b3574866408
Invigo <invigo.devel@gmail.com>
2023-10-16 00:38:53 +0300

    Update protein-translation.ts

--- protein-translation/protein-translation.ts
+++ protein-translation/protein-translation.ts
@@ -1,21 +1,12 @@
 const CODONS: { [codon: string]: string } = {
     AUG: "Methionine",
-    UUU: "Phenylalanine",
-    UUC: "Phenylalanine",
-    UUA: "Leucine",
-    UUG: "Leucine",
-    UCU: "Serine",
-    UCC: "Serine",
-    UCA: "Serine",
-    UCG: "Serine",
-    UAU: "Tyrosine",
-    UAC: "Tyrosine",
-    UGU: "Cysteine",
-    UGC: "Cysteine",
+    UUU: "Phenylalanine", UUC: "Phenylalanine",
+    UUA: "Leucine", UUG: "Leucine",
+    UCU: "Serine", UCC: "Serine", UCA: "Serine", UCG: "Serine",
+    UAU: "Tyrosine", UAC: "Tyrosine",
+    UGU: "Cysteine", UGC: "Cysteine",
     UGG: "Tryptophan",
-    UAA: "STOP",
-    UAG: "STOP",
-    UGA: "STOP",
+    UAA: "STOP", UAG: "STOP", UGA: "STOP",
   }
 
 export const translate = (rna: string = ""): string[] => {
