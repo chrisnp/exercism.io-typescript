4364658c2ac10bcf1c0cc32e9f9bc61aef2c6f3d
Invigo <invigo.devel@gmail.com>
2024-02-23 19:47:26 +0200

    Update resistor-color-trio.test.ts

--- resistor-color-trio/resistor-color-trio.test.ts
+++ resistor-color-trio/resistor-color-trio.test.ts
@@ -7,23 +7,49 @@ describe('Resistor Colors', () => {
     )
   })
 
-  it('Blue and grey and brown', () => {
+  xit('Blue and grey and brown', () => {
     expect(decodedResistorValue(['blue', 'grey', 'brown'])).toEqual('680 ohms')
   })
 
-  it('Red and black and red', () => {
+  xit('Red and black and red', () => {
     expect(decodedResistorValue(['red', 'black', 'red'])).toEqual('2 kiloohms')
   })
 
-  it('Green and brown and orange', () => {
+  xit('Green and brown and orange', () => {
     expect(decodedResistorValue(['green', 'brown', 'orange'])).toEqual(
       '51 kiloohms'
     )
   })
 
-  it('Yellow and violet and yellow', () => {
+  xit('Yellow and violet and yellow', () => {
     expect(decodedResistorValue(['yellow', 'violet', 'yellow'])).toEqual(
       '470 kiloohms'
+    )
+  })
+
+  xit('Blue and violet and blue', () => {
+    expect(decodedResistorValue(['blue', 'violet', 'blue'])).toEqual(
+      '67 megaohms'
+    )
+  })
+
+  xit('Minimum possible value', () => {
+    expect(decodedResistorValue(['black', 'black', 'black'])).toEqual('0 ohms')
+  })
+
+  xit('Maximum possible value', () => {
+    expect(decodedResistorValue(['white', 'white', 'white'])).toEqual(
+      '99 gigaohms'
+    )
+  })
+
+  xit('First two colors make an invalid octal number', () => {
+    expect(decodedResistorValue(['black', 'grey', 'black'])).toEqual('8 ohms')
+  })
+
+  xit('Ignore extra colors', () => {
+    expect(decodedResistorValue(['blue', 'green', 'yellow', 'orange'])).toEqual(
+      '650 kiloohms'
     )
   })
 })
