bc2ac4dee41b84fd6bd1a014dbeeed629a49c31e
Invigo <invigo.devel@gmail.com>
2024-04-11 00:22:06 +0300

    Refactor  rna-transcription.ts

--- rna-transcription/rna-transcription.ts
+++ rna-transcription/rna-transcription.ts
@@ -1,20 +1,11 @@
 const TRANSCRIBE: { [nucl: string]: string } = {
-    G: "C",
-    C: "G",
-    T: "A",
-    A: "U"
-};
+    G: 'C', C: 'G', T: 'A', A: 'U'
+}
 
-export default class Transcriptor {
-
-    toRna(dna: string): string {
-        if (/[^ACGT]/.test(dna)) {
-            throw new
-                Error("Invalid input DNA.");
-        }
-        return dna
-               .split("")
-               .map((n) => TRANSCRIBE[n])
-               .join("");
-    }
+export const toRna = (dna: string): string => {
+    if (/[^ACGT]/.test(dna))
+        throw new Error('Invalid input DNA.');
+    return dna.split('')
+              .map((n) => TRANSCRIBE[n])
+              .join('');
 }
