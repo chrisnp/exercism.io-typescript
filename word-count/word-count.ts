f65e0613e5658391500c9b5eabdceda515c75b6c
Invigo <invigo.devel@gmail.com>
2024-05-17 01:05:55 +0300

    Refactor word-count.ts (changed spec)

--- word-count/word-count.ts
+++ word-count/word-count.ts
@@ -1,7 +1,7 @@
 export const count = (sentence: String): Map<string, number> => 
-      sentence.trim().toLowerCase().split(/\s+/)
-              .reduce((freqs, word) => 
-                  freqs.set(word, (freqs.get(word) || 0) + 1), 
-                  new Map()
-              )
-
+    [...(sentence.trim().toLowerCase().match(/\b(\w|')+\b/g) ?? [])]
+    .reduce(
+        (freqs, word) => 
+            freqs.set(word, (freqs.get(word) || 0) + 1), 
+            new Map()
+     )
