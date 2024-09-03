8b0337b1721ab312fda858cc37256f41c415e653
Invigo <invigo.devel@gmail.com>
2024-09-03 23:45:34 +0300

    Update roman-numerals.ts

--- roman-numerals/roman-numerals.ts
+++ roman-numerals/roman-numerals.ts
@@ -1,3 +1,14 @@
-export const toRoman = () => {
-  throw new Error('Remove this statement and implement this function')
+const ROMAN = {
+  0: ['','I','II','III','IV','V','VI','VII','VIII','IX'],
+  1: ['', 'X', 'XX', 'XXX', 'XL','L','LX','LXX','LXXX','XC'],
+  2: ['','C', 'CC','CCC','CD','D','DC','DCC','DCCC','CM'],
+  3: ['','M','MM','MMM'] 
+}
+
+export const toRoman = (arabic: number): string => {
+  [...arabic.toString()]
+  .reverse().reduce(
+                (str, digit, power) => 
+                      (ROMAN[power])[digit] + str, ''
+             )
 }
