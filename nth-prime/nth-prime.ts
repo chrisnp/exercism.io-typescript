06dbf87ae5aaf1be8ee8bc064c6f99c74be86dd4
Invigo <invigo.devel@gmail.com>
2024-09-25 23:32:06 +0300

    nth-prime.ts -- passing all

--- nth-prime/nth-prime.ts
+++ nth-prime/nth-prime.ts
@@ -1,5 +1,24 @@
-
-
-export function nth() {
-  throw new Error('Remove this statement and implement this function')
+const sieve = (upto: number): number[] => {
+  let p = 2
+  const range = [], primes = []
+  for (let i = 2; i <= upto; i++) 
+      range[i] = {val: i, prime: true}
+  for (let i = 2; i <= upto / 2; i++) {
+      for (let j = 2 * p; j <= upto; j += p) {
+          range[j].prime = false
+      }
+      p++
+  }
+  for (let k in range)
+      range[k].prime ? primes.push(Number(k)) : null
+  return primes
 }
+
+export const nth = (n: number): number => {
+  if (n < 1) throw new Error("Prime is not possible")
+  let approximate = 
+        n * Math.log(n) + n * Math.log(Math.log(n))
+  approximate < 15 ? approximate = 15 : {}
+  return sieve(approximate)[n - 1]
+}
+
