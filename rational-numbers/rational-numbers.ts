9e7fbdb3e50810cc2ec5084f69765a7c5a629e53
Invigo <invigo.devel@gmail.com>
2024-10-30 23:58:18 +0200

    Update rational-numbers.ts

--- rational-numbers/rational-numbers.ts
+++ rational-numbers/rational-numbers.ts
@@ -1,37 +1,74 @@
+function gcd(x: number, y: number): number {
+  return y ? gcd(y, x % y) : x
+}
+
+const nthRoot = (base: number, n: number) => Math.pow(base, 1 / n) 
+
 export class Rational {
-  constructor() {
-    throw new Error('Remove this statement and implement this function')
+  numerator: number
+  denominator: number
+
+  constructor(numerator: number, denominator: number = 1) {
+    if (denominator === 0) throw "Can't divide by zero"
+    this.numerator = numerator
+    this.denominator = denominator
+    this.reduce()
   }
 
-  add() {
-    throw new Error('Remove this statement and implement this function')
+  add(other: Rational): Rational {
+    const numerator = 
+      this.numerator * other.denominator + other.numerator * this.denominator;
+    const denominator = 
+      this.denominator * other.denominator;
+    return new Rational(numerator, denominator);
   }
 
-  sub() {
-    throw new Error('Remove this statement and implement this function')
+  sub(other: Rational): Rational {
+    const numerator = 
+      this.numerator * other.denominator - other.numerator * this.denominator;
+    const denominator = 
+      this.denominator * other.denominator;
+    return new Rational(numerator, denominator);
   }
 
-  mul() {
-    throw new Error('Remove this statement and implement this function')
+  mul(other: Rational): Rational {
+    const numerator = this.numerator * other.numerator;
+    const denominator = this.denominator * other.denominator;
+    return new Rational(numerator, denominator);
   }
 
-  div() {
-    throw new Error('Remove this statement and implement this function')
+  div(other: Rational): Rational {
+    const numerator = this.numerator * other.denominator;
+    const denominator = this.denominator * other.numerator;
+    return new Rational(numerator, denominator);
   }
 
-  abs() {
-    throw new Error('Remove this statement and implement this function')
+  abs(): Rational {
+    return new Rational(Math.abs(this.numerator), Math.abs(this.denominator))
   }
 
-  exprational() {
-    throw new Error('Remove this statement and implement this function')
+  exprational(exponent: number): Rational {
+    if (exponent < 0) {
+      exponent = -exponent
+      return new Rational(this.denominator ** exponent, this.numerator ** exponent)
+    } else {
+      return new Rational(this.numerator ** exponent, this.denominator ** exponent)
+    }
   }
 
-  expreal() {
-    throw new Error('Remove this statement and implement this function')
+  expreal(real: number): number {
+    return Math.pow(real ** (1/this.denominator), this.numerator);
   }
 
-  reduce() {
-    throw new Error('Remove this statement and implement this function')
+  toReal(): number {
+    return this.numerator / this.denominator;
+  }
+
+  reduce(): Rational {
+    const divisor = gcd(this.numerator, this.denominator)
+    return new Rational(
+                  this.numerator / divisor, 
+                  this.denominator / divisor
+               )
   }
 }
