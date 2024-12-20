977ae5d351e55f5a2fe0c54d4b2070de494ffd91
Invigo <invigo.devel@gmail.com>
2024-12-20 23:56:44 +0200

    Reformat & retest bob.ts

--- bob/bob.ts
+++ bob/bob.ts
@@ -1,19 +1,20 @@
-const shouting = (message: string):boolean =>
-            message === message.toUpperCase() && 
-            (/[A-Z]+/).test(message),
-      question = (message: string):boolean =>
-            message.trim().endsWith('?'),
-      silence  = (message: string):boolean =>
-            !message.trim(),
-      forceful = (message: string):boolean =>
-            shouting(message) && question(message)
-
+const shouting = 
+            (message: string):boolean => 
+                  message === message.toUpperCase() && 
+                  (/[A-Z]+/).test(message),
+      question = 
+            (message: string):boolean => 
+                  message.trim().endsWith('?'),
+      silence  = 
+            (message: string):boolean => 
+                  !message.trim(),
+      forceful = 
+            (message: string):boolean => 
+                  shouting(message) && question(message)
 
 export const hey = (message: string): string => 
-            forceful(message) ? 
-                "Calm down, I know what I'm doing!"  :
-            shouting(message) ? "Whoa, chill out!"   :
-            question(message) ? "Sure."              :
-            silence (message) ? "Fine. Be that way!" :
-                                "Whatever."
+            forceful(message) ? "Calm down, I know what I'm doing!" : 
+            shouting(message) ? "Whoa, chill out!"                  : 
+            question(message) ? "Sure."                             : 
+            silence (message) ? "Fine. Be that way!" : "Whatever."
 
