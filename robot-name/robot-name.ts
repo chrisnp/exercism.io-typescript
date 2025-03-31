49d8dddb733a4b6464c8940c8827267d2d0fa608
Invigo <invigo.devel@gmail.com>
2025-03-31 22:30:52 +0300

    Refactor robot-name.ts

--- robot-name/robot-name.ts
+++ robot-name/robot-name.ts
@@ -1,13 +1,6 @@
-const randomAlpha = () => 
-    String.fromCharCode(Math.floor(Math.random() * 26) + 65) +
-    String.fromCharCode(Math.floor(Math.random() * 26) + 65)
-
-const randomNumeric = () => Math.floor(Math.random() * 1000)
-                                .toString().padStart(3, "0")
-
 export class Robot {
-
   private _name: string = ""
+
   static used = new Set()
 
   constructor() { this.resetName() }
@@ -23,9 +16,16 @@ export class Robot {
     } while (this.used.has(name))
     this.used.add(name)
     return name
-}
+  }
 
   public static releaseNames(): void {
     this.used.clear()
   }
 }
+
+const randomAlpha = () => 
+  String.fromCharCode(Math.floor(Math.random() * 26) + 65) +
+  String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+
+const randomNumeric = () => Math.floor(Math.random() * 1000)
+                                .toString().padStart(3, "0")
