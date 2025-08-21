27ee2da6d468c9e783bbf03ed8b680d8940b5827
Invigo <invigo.devel@gmail.com>
2025-08-21 22:31:21 +0300

    Update pythagorean-triplet.ts

--- pythagorean-triplet/pythagorean-triplet.ts
+++ pythagorean-triplet/pythagorean-triplet.ts
@@ -1,22 +1,3 @@
-// type Options = {
-//   minFactor?: number
-//   maxFactor?: number
-//   sum: number
-// }
-
-// export function triplets({}: Options): Triplet[] {
-//   throw new Error('Remove this statement and implement this function')
-// }
-
-// class Triplet {
-//   constructor() {
-//     throw new Error('Remove this statement and implement this function')
-//   }
-
-//   toArray(): [number, number, number] {
-//     throw new Error('Remove this statement and implement this function')
-//   }
-// }
 type Options = {
   minFactor?: number
   maxFactor?: number
